a76852d800c9dffeada2c636835c043601309ac6
MuhilKumaran <137814562+MuhilKumaran@users.noreply.github.com>
2024-01-27 18:03:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function popup() {
+  swal({
+    title: "Booking Confirmed",
+    text: "Thank You",
+    icon: "success",
+    button: "OK",
+  });
+}
+
+function loginPopup() {
+  swal({
+    title: "Login  Successfull",
+    text: "Thank You for Choosing Us",
+    icon: "success",
+    button: "Done",
+  });
+}
+
+function signUpPopup() {
+  swal({
+    title: "SignUP  Successfull",
+    text: "Registered Successfully",
+    icon: "success",
+    button: "OK",
+  });
+}
